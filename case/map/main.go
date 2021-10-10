8ef5aff69177bda298606b0998abb4a7e549c702
wbing <441282413@qq.com>
2021-10-10 22:42:06 +0800

    go.mod模块的理解

--- case/map/main.go
+++ case/map/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/wbing441282413/gotest/case/map/map01"
-	"github.com/wbing441282413/gotest/case/struct/student"
 )
 
 //好像必须写完整，不然会报错
@@ -24,8 +21,13 @@ func main() {
 	map01.BuildIndex(lis)     //构建基于查询的组合键（name、age)
 	map01.QueryData("张三", 23) //依据name、age进行查询（多条件）
 
-	stu := new(student.Student)
-	stu.Name = "王兵"
-	fmt.Println(stu.Name)
+	/**
+	想在这里用mod引入另一个模块struct的student，好像不太容易，会出现ambiguous import的问题，因为
+	如果要把项目分成多个模块，那每个模块都应该有自己的单独的tag，并且在引入的时候，require的时候要把tag指明
+	这样才不会出现ambiguous import的问题，可以看下https://zhuanlan.zhihu.com/p/134184461，有说到
+	*/
+	// stu := new(student.Student)
+	// stu.Name = "王兵"
+	// fmt.Println(stu.Name)
 
 }
